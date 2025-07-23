a2812a68f8ab9e4dca293aca316614b378208b55
dq-aem-saraswathi <saraswathik@digiquadsolutions.com>
2025-07-23 11:09:10 +0530

    adding templates to the new project

--- src/main/java/com/aem/builder/controller/AemProjectController.java
+++ src/main/java/com/aem/builder/controller/AemProjectController.java
@@ -1,6 +1,7 @@
 package com.aem.builder.controller;
 
 import com.aem.builder.model.AemProjectModel;
+import com.aem.builder.service.TemplateService;
 import com.aem.builder.service.impl.AemProjectServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -10,25 +11,31 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.io.IOException;
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class AemProjectController {
 
 
     private final AemProjectServiceImpl aemProjectService;
-
+    private final TemplateService templateService;
     @GetMapping("/create")
     public String test(Model model) throws IOException {
-
+        List<String> templates = templateService.getTemplateFileNames();
+        model.addAttribute("templates", templates);
         model.addAttribute("aemProjectModel", new AemProjectModel());
         return "create";
-
     }
 
     @PostMapping("/save")
-    public String saveConfig(@ModelAttribute AemProjectModel aemProjectModel, Model model) {
+    public String saveConfig(@ModelAttribute AemProjectModel aemProjectModel, Model model) throws IOException {
         aemProjectService.generateAemProject(aemProjectModel);
         model.addAttribute("message", "AEM Project created successfully under generated-projects directory.");
+        templateService.copySelectedTemplatesToGeneratedProject(
+                aemProjectModel.getProjectName(),
+                aemProjectModel.getSelectedTemplates()
+        );
         return "dashboard";
     }
 }
--- src/main/java/com/aem/builder/service/TemplateService.java
+++ src/main/java/com/aem/builder/service/TemplateService.java
@@ -1,4 +1,12 @@
 package com.aem.builder.service;
 
+import java.io.IOException;
+import java.util.List;
+
 public interface TemplateService {
+    public List<String> getTemplateFileNames() throws IOException;
+    public void copySelectedTemplatesToGeneratedProject(String projectName, List<String> selectedTemplates) throws IOException;
+    public List<String>getDistinctTemplates(String projectname,List<String>resourceTemplates,List<String>projectTemplates);
+    public List<String>getCommonTemplates(List<String>resourcetemplates,List<String>projectTemplates);
+    public List<String> getTemplateNamesFromDestination(String projectName);
 }
