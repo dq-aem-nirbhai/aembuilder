5d0893c7a8679ccf063d5d8128ab9c1d3aa3cf01
Nirbhai <nirbhaip@digiquadsolutions.com>
2025-08-04 10:48:37 +0530

    Add existing projects listing and fix controller

--- src/main/java/com/aem/builder/controller/HomeController.java
+++ src/main/java/com/aem/builder/controller/HomeController.java
@@ -1,13 +1,9 @@
 package com.aem.builder.controller;
 
-import com.aem.builder.model.AemProjectModel;
-import com.aem.builder.service.impl.AemProjectServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import java.io.IOException;
 
--- src/main/java/com/aem/builder/service/AemProjectService.java
+++ src/main/java/com/aem/builder/service/AemProjectService.java
@@ -3,7 +3,18 @@ package com.aem.builder.service;
 
 import com.aem.builder.model.AemProjectModel;
 
+import java.util.List;
+
 public interface AemProjectService {
 
     void generateAemProject(AemProjectModel aemProjectModel);
+
+    /**
+     * Returns the names of AEM projects that have already been generated
+     * in the workspace. This is used by the UI to warn users if they are
+     * about to create a project that already exists.
+     *
+     * @return list of project directory names, never {@code null}
+     */
+    List<String> getExistingProjects();
 }
