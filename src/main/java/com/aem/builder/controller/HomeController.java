3ab0d80040589d895b2e598d1227bbb4ac3d24f0
dq-aem-bhagyalaxmi <bhagyach@digiquadsolutions.com>
2025-08-04 12:18:16 +0530

    some changes has been done
    importProject code changes has been removed. now its working fine

--- src/main/java/com/aem/builder/controller/HomeController.java
+++ src/main/java/com/aem/builder/controller/HomeController.java
@@ -6,10 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.util.List;
@@ -27,22 +23,5 @@ public class HomeController {
         return "dashboard";
     }
 
-    @PostMapping("/import")
-    public String importProject(@RequestParam("file") MultipartFile file,
-                                RedirectAttributes redirectAttributes) {
-        if (file.isEmpty()) {
-            redirectAttributes.addFlashAttribute("error", "Please select a file to upload.");
-            return "redirect:/";
-        }
-
-        try {
-            aemProjectService.importAemProject(file);
-            redirectAttributes.addFlashAttribute("message", "Project imported successfully.");
-        } catch (Exception e) {
-            redirectAttributes.addFlashAttribute("error", "Failed to import project: " + e.getMessage());
-        }
-
-        return "redirect:/";
-    }
 
 }
--- src/main/java/com/aem/builder/service/AemProjectService.java
+++ src/main/java/com/aem/builder/service/AemProjectService.java
@@ -4,13 +4,10 @@ package com.aem.builder.service;
 import com.aem.builder.model.AemProjectModel;
 import com.aem.builder.model.ProjectDetails;
 
-import org.springframework.web.multipart.MultipartFile;
-
 import java.util.List;
 
 public interface AemProjectService {
     void generateAemProject(AemProjectModel aemProjectModel);
     List<ProjectDetails> getAllProjects();
-    void importAemProject(MultipartFile file) throws Exception;
 
 }
