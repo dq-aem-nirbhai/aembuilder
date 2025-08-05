238aca8b9c502a40d9b98653fd867f802355541e
dq-aem-anilkumar <anilkumarg@digiquadsolutions.com>
2025-08-05 18:18:01 +0530

    UI changes in the AEM builder Tool and added index_page

--- src/main/java/com/aem/builder/controller/HomeController.java
+++ src/main/java/com/aem/builder/controller/HomeController.java
@@ -17,6 +17,12 @@ public class HomeController {
     private final AemProjectServiceImpl aemProjectService;
 
     @GetMapping("/")
+    public String index(Model model)  {
+
+        return "indexpage";
+    }
+
+    @GetMapping("/dashboard")
     public String test(Model model) throws IOException {
         List<ProjectDetails> projects = aemProjectService.getAllProjects();
         model.addAttribute("projects", projects);
