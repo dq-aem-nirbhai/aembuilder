d9641288655af12e802d63221facb8bc91cc2287
manojkumarmengani <manojm@digiquadsolutions.com>
2025-08-07 15:04:27 +0530

    add support for creating and updating component policies,implement policy editor UI with dynamic style group and style support

--- src/main/java/com/aem/builder/model/PolicyModel.java
+++ src/main/java/com/aem/builder/model/PolicyModel.java
@@ -1,14 +1,9 @@
-package com.aem.builder.model.policy;
+package com.aem.builder.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.w3c.dom.Node;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Represents an AEM component policy with style groups.
@@ -16,7 +11,7 @@ import java.util.Map;
 @Data
 @RequiredArgsConstructor
 public class PolicyModel {
-    private String id; // folder name of policy
+    private String id;
     private String title;
     private String description;
     private String defaultCssClass;
