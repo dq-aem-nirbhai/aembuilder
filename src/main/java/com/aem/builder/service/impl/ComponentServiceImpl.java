2d4434c1907ba02e39af7996d2d12557460cb646
dq-aem-jay <jayp@digiquadsolutions.com>
2025-09-09 11:54:33 +0530

    Fixing design dialog issue for core components

--- src/main/java/com/aem/builder/service/impl/ComponentServiceImpl.java
+++ src/main/java/com/aem/builder/service/impl/ComponentServiceImpl.java
@@ -1105,7 +1105,7 @@ Updating logic below
         return null;
     }
 
-    private String searchComponentRecursiveExact(File dir, String componentName) {
+    public String searchComponentRecursiveExact(File dir, String componentName) {
         if (!dir.isDirectory()) return null;
 
         // Exact case-sensitive match
