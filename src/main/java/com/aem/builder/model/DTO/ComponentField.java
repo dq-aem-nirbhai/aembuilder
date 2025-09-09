e367970f5da7a338908f61614a24d606bd68dbff
manojkumarmengani <manojm@digiquadsolutions.com>
2025-09-09 14:28:45 +0530

    Read superType dialog to fetch parent tabs and generate tab structures based on selected tabs.

--- src/main/java/com/aem/builder/model/DTO/ComponentField.java
+++ src/main/java/com/aem/builder/model/DTO/ComponentField.java
@@ -1,6 +1,7 @@
 package com.aem.builder.model.DTO;
 
 import com.aem.builder.model.Enum.FieldType;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,6 +21,8 @@ public class ComponentField {
     private String fieldLabel;                 // e.g., Title
     private String fieldName;                  // e.g., title
     private String fieldType;                  // from FieldType enum
+    @JsonProperty("isParentTab")
+    private boolean isParentTab;
     private List<ComponentField> nestedFields; // only for multifields
     private List<OptionItem> options;          // for select/checkboxgroup values
 
