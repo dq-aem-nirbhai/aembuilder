252ea8d5fc07685000dd0b37a5af4f0229f269ac
manojkumarmengani <manojm@digiquadsolutions.com>
2025-09-12 18:09:41 +0530

    standardize DeployServiceImpl with constants, static imports, and logging,Refactored HomeController to use direct static imports for URLs, views, and model keys

--- src/main/java/com/aem/builder/constants/ViewNames.java
+++ src/main/java/com/aem/builder/constants/ViewNames.java
@@ -1,4 +1,14 @@
 package com.aem.builder.constants;
 
-public class ViewNames {
+public final class ViewNames {
+    /* Page Redirections...*/
+    public static final String CREATE_PAGE = "create";
+    public static final String DEPLOY_PAGE = "deploy";
+    public static final String DEPLOY_LOGS_PAGE = "deployLogs";
+    public static final String ERROR_PAGE= "error";
+    public static final String INDEX_PAGE = "indexpage";
+    public static final String DASHBOARD_PAGE="dashboard";
+    /*URL Redirections...*/
+    public static final String DASHBOARD_REDIRECT = "redirect:/dashboard";
 }
+
