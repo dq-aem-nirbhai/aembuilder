17b1d776d21f6b6d6e565a0ea985398a2b96b0d6
manojkumarmengani <manojm@digiquadsolutions.com>
2025-09-15 16:37:38 +0530

    chore: update constants and controller before merge

--- src/main/java/com/aem/builder/constants/ModelAttributeKeys.java
+++ src/main/java/com/aem/builder/constants/ModelAttributeKeys.java
@@ -7,4 +7,17 @@ public  final class ModelAttributeKeys {
     public static final String TEMPLATES = "templates";
     public static final String COMPONENT_LIST = "componentList";
     public static final String AEM_PROJECT_MODEL = "aemProjectModel";
+    public static final String PROJECT_NAME = "projectName";
+    public static final String BRANCH = "branch";
+    public static final String BRANCHES = "branches";
+    public static final String HAS_STASH = "hasStash";
+    public static final String COMPONENTS = "components";
+    public static final String EDITABLE_COMPONENTS = "editableComponents";
+    public static final String CAN_DEPLOY = "canDeploy";
+    public static final String PROJECTS = "projects";
+    public static final String CONFLICTS = "conflicts";
+    public static final String TARGET_BRANCH = "targetBranch";
+    public static final String BRANCH_ACTION = "branchAction";
+    public static final String FROM_BRANCH = "fromBranch";
+
 }
--- src/main/java/com/aem/builder/constants/UrlMappings.java
+++ src/main/java/com/aem/builder/constants/UrlMappings.java
@@ -6,6 +6,25 @@ public final class UrlMappings {
     public static final String CREATE_PROJECT_URL = "/create";
     public static final String CHECK_PROJECT_NAME_URL = "/checkProjectName";
     public static final String SAVE_PROJECT_URL = "/save";
-    // Dashboard
     public static final String DASHBOARD_URL = "/dashboard";
+    public static final String VIEW_PROJECT_URL = "/view/{projectName}";
+    public static final String DEPLOY_PROJECT_URL= "/{projectName}/deploy";
+    public static final String DEPLOY_LOGS_URL = "/{projectName}/deploy/logs";
+    public static final String DOWNLOAD_PROJECT_URL = "/download/{projectName}";
+    public static final String IMPORT_PROJECT_URL = "/import";
+    public static final String CLONE_PROJECT_URL= "/clone";
+    public static final String VALIDATE_IMPORT_URL = "/validateImport";
+    public static final String DELETE_PROJECT_URL = "/delete/{projectName}";
+
+    // ------------------- GIT BRANCH OPERATIONS -------------------
+
+    public static final String SWITCH_BRANCH_URL= "/{projectName}/switchBranch";
+    public static final String RESOLVE_CONFLICTS_URL = "/{projectName}/resolveConflicts";
+    public static final String UNSTASH_URL = "/{projectName}/unstash";
+    public static final String CREATE_BRANCH_URL= "/{projectName}/createBranch";
+
+    // ------------------- REDIRECT -------------------
+    public static final String REDIRECT_VIEW_PROJECT_URL = "redirect:/view/{projectName}";
+
+    public static final String SHOW_FOLDER_URL = "/show-folder";
 }
